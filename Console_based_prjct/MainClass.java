c6f2fc0bf3bb4d9368caa0550c6580f0ccd870a6
Bharathi225 <120001195+Bharathi225@users.noreply.github.com>
2023-02-11 12:56:03 +0530

    Add files via upload

---
+++ Console_based_prjct/MainClass.java
@@ -0,0 +1,12 @@
+package Console_based_prjct;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		//			Scanner scan = new Scanner(System.in);
+		//Object is created for Student class
+		Student student = new Student();
+	}
+}
+
+
